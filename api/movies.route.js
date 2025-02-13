b91129c8f6a142394e9e50d99d247a0d36f97469
Diego Hills <hillsdiego33@gmail.com>
2025-02-13 14:37:23 +0200

    updated commits

--- api/movies.route.js
+++ api/movies.route.js
@@ -1,13 +1,23 @@
-import express from 'express' 
-import MoviesController from './movies.controller.js' 
-import ReviewsController from './reviews.controller.js'  
-const router = express.Router() // get access to express router 
+import express from 'express'
+import MoviesController from './movies.controller.js'
+import ReviewsController from './reviews.controller.js' 
+
+// get access to express router
+const router = express.Router() 
+
+//router.route('/').get((req,res) => res.send('hello world'))
+//gets full movie object
 router.route('/').get(MoviesController.apiGetMovies)
-router.route("/id/:id").get(MoviesController.apiGetMovieById) 
+
+//route gets specific movies and their ratings
+router.route("/id/:id").get(MoviesController.apiGetMovieById)
 router.route("/ratings").get(MoviesController.apiGetRatings)
-router 
-.route("/review") 
-.post(ReviewsController.apiPostReview) 
-.put(ReviewsController.apiUpdateReview) 
-.delete(ReviewsController.apiDeleteReview)   
-export default router 
+
+//CRUD functioning for reviews
+router
+    .route("/review")
+    .post(ReviewsController.apiPostReview)
+    .put(ReviewsController.apiUpdateReview)
+    .delete(ReviewsController.apiDeleteReview)
+
+export default router
